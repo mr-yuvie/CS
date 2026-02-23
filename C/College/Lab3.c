347d1b863c539896d7902371f08b9881dcb3f619
mr_yuvie <yuvjindal@gmail.com>
2026-02-23 20:15:30 +0530

    Updated Lab3

--- C/College/Lab3.c
+++ C/College/Lab3.c
@@ -1,23 +1,26 @@
+#include <math.h>
 #include <stdio.h>
 
-void towerOfHanoi(int n, char source, char auxiliary, char destination) {
+void ToH(int n, char source, char auxiliary, char destination) {
     if (n == 1) {
         printf("Move disk 1 from %c to %c\n", source, destination);
         return;
     }
 
-    towerOfHanoi(n - 1, source, destination, auxiliary);
+    ToH(n - 1, source, destination, auxiliary);
     printf("Move disk %d from %c to %c\n", n, source, destination);
-    towerOfHanoi(n - 1, auxiliary, source, destination);
+    ToH(n - 1, auxiliary, source, destination);
 }
 
 int main() {
     int n;
+    int total_steps;
 
     printf("Enter number of disks: ");
     scanf("%d", &n);
 
-    towerOfHanoi(n, 'A', 'B', 'C');
-
+    ToH(n, 'S', 'A', 'D');
+    total_steps = pow(2, n) - 1;
+    printf("Total steps: %d", total_steps);
     return 0;
 }
